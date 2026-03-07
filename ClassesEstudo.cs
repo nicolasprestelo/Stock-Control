eb49d9b86042900809d5bc59a39b03e601a57464
nicolasprestelo <nicolasprestelorocha@gmail.com>
2026-03-07 14:16:53 -0300

    Adicionar arquivos de projeto.

---
+++ ClassesEstudo.cs
@@ -0,0 +1,55 @@
+﻿
+class Produto
+{
+     public Produto(string nome, int estoque)
+    {
+        Nome = nome;
+        if (estoque >= 0)
+        {
+            Estoque = estoque;
+        }
+        else
+        {
+            Estoque = 0;
+        }
+    }
+
+    public string Nome { get;}
+    public int Estoque { get; private set; }
+
+    public decimal Preco { get; private set; }
+
+    public void AlterarPreco(decimal novoPreco)
+    {
+        if (novoPreco > 0)
+        {
+            Preco = novoPreco;
+        }
+    }
+
+    public void AdicionarEstoque(int quantidade)
+    {
+        if (quantidade > 0)
+        {
+            Estoque += quantidade;
+        }
+    }
+
+    public void RemoverEstoque (int quantidade)
+    {
+        if (quantidade < 0) 
+        {
+            Console.WriteLine("Insira um número positivo para que haja a remoção do estoque.");
+        }
+        else if (quantidade > Estoque) 
+        {
+            Console.WriteLine($"Infelizmente só temos {Estoque} unidades em estoque.");
+        }
+        else if (quantidade > 0)
+        {
+            Estoque -= quantidade;      
+        }
+    }
+    public void ExibirInformacoes() =>
+    Console.WriteLine($" Nome do produto: {Nome}\n Preço: {Preco}R$\n Quantidade em Estoque: {Estoque}");
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿var produtos = new Dictionary<string, Produto>();
+
+
+void MenuInicial()
+{
+    Console.WriteLine("Bem-vindo ao controle de estoque de seus produtos!!");
+    Console.WriteLine("1 - Cadastrar produto");
+    Console.WriteLine("2 - Alterar preço");
+    Console.WriteLine("3 - Adicionar estoque");
+    Console.WriteLine("4 - Remover estoque");
+    Console.WriteLine("5 - Listar produtos");
+    Console.WriteLine("0 - Sair");
+    Console.Write("Digite a sua opção: ");
+    int resposta = int.Parse(Console.ReadLine());
+
+    switch (resposta)
+    {
+        case 1: CadastroDeProdutos();
+            break;
+        case 2: AlteracaoDePreco();
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break; 
+        case 6:
+            break;
+    }
+}
+
+void CadastroDeProdutos()
+{
+    Thread.Sleep(2000);
+    Console.Clear();
+
+    Console.Write("Nome do produto: ");
+    string nome = Console.ReadLine();
+    Console.Write("Quantidade inicial em estoque: ");
+    int estoque = int.Parse(Console.ReadLine());
+    Produto novoProduto = new Produto(nome, estoque);
+    produtos.Add(nome, novoProduto);
+
+    limparConsole();
+    MenuInicial();
+}
+
+void AlteracaoDePreco()
+{
+    Thread.Sleep(2000);  
+    Console.Clear();
+
+    Console.Write("Qual produto deseja alterar o preço? ");
+    string nomeProduto = Console.ReadLine();
+    //Verificar se tem no dictionary
+
+    if (produtos.ContainsKey(nomeProduto))
+    {
+        Produto produtoAlterarPreco = produtos[nomeProduto];
+
+        Console.Write("Novo preço: ");
+        decimal novoPreco = decimal.Parse(Console.ReadLine());
+         produtoAlterarPreco.AlterarPreco(novoPreco);
+    }
+
+    //se nao, nao encontrou
+    else
+    {
+        Console.WriteLine("Produto não encontrado.");
+    }
+
+    limparConsole();
+    MenuInicial();
+}
+void limparConsole()
+{
+    Console.WriteLine("Digite qualquer tecla para voltar ao menu inicial: ");
+    Console.ReadKey();
+    Thread.Sleep(2000);
+    Console.Clear();
+}
+MenuInicial();
